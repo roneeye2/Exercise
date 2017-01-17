77397747cdf1544a980f7056545d7ebb8db8cc93
roneeye2 <roneeye@gmail.com>
2017-01-17 17:58:15 +0530

    trl

---
+++ Shared/GloboMart.Framwork.Entity/ProductPriceSchedule.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GloboMart.Framwork.Entity
+{
+    public partial class ProductPriceSchedule : BaseEntity
+    {
+        public DateTime StartDate { get; set; }
+        public DateTime EndDate { get; set; }
+        public int CurrencyId { get; set; }
+    }
+}
